ecd549ed2540b065d11924ff81a4c39e3fd12262
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-09-25 13:02:10 +1000

    set up file structure for routes, added thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    thoughtsById, 
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction, 
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+//All thought routes
+router
+    .route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+//Thought route by id
+router
+    .route('/:id')
+    .get(thoughtsById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// All reaction routes by thought ID
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+
+// Reaction route for specific reaction via thought
+router 
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
