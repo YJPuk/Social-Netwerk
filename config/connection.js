3982693236bdad71f91daf2df96b30db0e3ce2b0
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-09-25 15:28:56 +1000

    added some fixes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,11 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/social-netwerk';
 
-connect(connectionString);
+// To ensure MongoDB connection is using the latest recommended features
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
 module.exports = connection;
